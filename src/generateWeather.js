0ac2158eb4a534b91e36344c6b2306fd97de62fd
William Gomez <89489831+WilliamG-1@users.noreply.github.com>
2023-03-15 10:29:20 -0700

    Created function to get api information, then call InfoAppender to create a new dom element

---
+++ src/generateWeather.js
@@ -0,0 +1,29 @@
+import createWeatherNode from "./infoAppender";
+
+const APIKEY = 'ae6cff0c5dec4b33010703d6fac46a44';
+
+const Location = (name, temperature, weatherDescription) => {
+    return {name, temperature, weatherDescription};
+};
+
+async function getLocationResponse(location)
+{
+    const apiRequest = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${APIKEY}`;
+    const promise = await fetch(apiRequest, {mode: 'cors'});
+    const response = await promise.json();
+    return response;
+}
+
+
+
+function getLocationWeatherInformation(location)
+{
+    (async () => {
+        const p = await getLocationResponse(location);
+        const newLocation = Location(p.name, p.main.temp, p.weather[0].description);
+        createWeatherNode(document.body, newLocation);
+    })();  
+}
+
+export default getLocationWeatherInformation;
+
