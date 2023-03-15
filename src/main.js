4b49e0eeb8fad7ff668b68ea1ab0a65afe7bff3f
William Gomez <89489831+WilliamG-1@users.noreply.github.com>
2023-03-15 10:29:40 -0700

    Succesfully cale getLocationWeather function

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import './style.css';
+import getLocationWeatherInformation from './generateWeather';
+
 
 const test = document.createElement('div');
 test.textContent = "This is an up and coming weather app lol";
 
 document.body.appendChild(test);
+
+const test2 = document.createElement('div');
+document.body.appendChild(test2);
+
+getLocationWeatherInformation('London');
+
+
