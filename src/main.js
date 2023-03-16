345e08ba616b041a96aa30615bbe051f05cb87bf
William Gomez <89489831+WilliamG-1@users.noreply.github.com>
2023-03-15 17:09:33 -0700

    Added serach function for searching location and retrieving data

--- src/main.js
+++ src/main.js
@@ -1,9 +1,28 @@
 import './style.css';
 import getLocationWeatherInformation from './generateWeather';
-
+import s from './assets/search.svg';
 
 const mainWeatherInfo = document.querySelector('#mainWeather');
-
+const searchButton = document.querySelector('#searchButton');
+const searchBar = document.querySelector('#searchBar');
+const form = document.querySelector('form');
 getLocationWeatherInformation('London', mainWeatherInfo);
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    resetWeatherCard();
+    const location = searchBar.value;
+    searchBar.value = '';
+    console.log(location);
+    getLocationWeatherInformation(location, mainWeatherInfo);
+    
+});
+
+function resetWeatherCard()
+{
+    while (mainWeatherInfo.hasChildNodes())
+    {
+        mainWeatherInfo.removeChild(mainWeatherInfo.lastChild);
+    }
+}
 
